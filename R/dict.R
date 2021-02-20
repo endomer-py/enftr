aed75bfa6f10cbe980b23f9d4481289f2360b984
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-02-20 17:45:02 -0400

    feat: diccionario y etiquetas de datos mejorados

---
+++ R/dict.R
@@ -0,0 +1,8 @@
+#' Diccionario para los datos de la ENFT
+#'
+#'   Contiene las etiquetas tanto para las variables como para los valores de estas.
+#'
+#' @format [list]
+#'
+#' @source \url{https://github.com/endomer/enftr/tree/master/Files/Cuestionarios/}
+"dict"
